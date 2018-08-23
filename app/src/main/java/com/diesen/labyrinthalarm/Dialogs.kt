e01cb2e8039a9926364cf26df15b6ba4b9fe99ca
k_oyama <diesen.aruzu65@gmail.com>
2018-08-23 18:23:46 +0900

    add kotlin files

---
+++ app/src/main/java/com/diesen/labyrinthalarm/Dialogs.kt
@@ -0,0 +1,66 @@
+package com.diesen.labyrinthalarm
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.widget.DatePicker
+import android.widget.TimePicker
+import java.util.*
+
+class DatePickFragment : DialogFragment(), DatePickerDialog.OnDateSetListener{
+
+    interface OnDateSelectedListener {
+        fun onSelected(year: Int, month: Int, date: Int)
+    }
+
+    private lateinit var listener: OnDateSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is OnDateSelectedListener) {
+            listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DATE)
+        return DatePickerDialog(context, this , year, month ,date)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, date: Int) {
+        listener.onSelected(year, month, date)
+    }
+}
+
+class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    interface OnTimeSelectedListener {
+        fun onSelected(hourOfDay: Int, minute: Int)
+    }
+
+    private lateinit var listener: OnTimeSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is TimePickerFragment.OnTimeSelectedListener) {
+            listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+        return TimePickerDialog(context, this, hour, minute, true)
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        listener.onSelected(hourOfDay, minute)
+    }
+}
